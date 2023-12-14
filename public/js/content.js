33cba7feacd125a0fc2cb76e048c64119326636a
Shenhaoyi <shenhaoyi04@163.com>
2023-12-15 00:12:04 +0800

    初步支持自定义样式

--- public/js/content.js
+++ public/js/content.js
@@ -13,3 +13,14 @@ script.onload = function () {
 };
 
 appendScript();
+
+// 临时使用，TODO:移除
+const currentUrl = window.location.href;
+if (currentUrl.includes('bilibili')) {
+  const link = document.createElement('link');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  link.href = chrome.runtime.getURL('/css/bilibili.css');
+  const head = document.getElementsByTagName('head')[0];
+  head.appendChild(link);
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,12 @@
-import { siteSelectors } from './config.js';
+import { siteSelectors, customStyleSites } from './config.js';
 import generateHiddenCssCode from './utils/generateHiddenCssCode.js';
 import loadCssCode from './utils/loadCssCode.js';
+import loadStyle from './utils/loadStyle.js';
 
 const doit = () => {
   console.log('doit');
   const currentUrl = window.location.href;
+  // 1.隐藏元素
   for (let key in siteSelectors) {
     if (currentUrl.includes(key)) {
       const selectors = siteSelectors[key];
@@ -12,5 +14,9 @@ const doit = () => {
       loadCssCode(code);
     }
   }
+  // 2.加载自定义样式,TODO:无法访问 chrome API，待完善
+  // customStyleSites.forEach((site) => {
+  //   loadStyle(`/css/${site}.css`);
+  // });
 };
 doit();
