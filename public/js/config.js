19382eb447c8a95cfb95806de849e34513cd0cb7
Shenhaoyi <shenhaoyi04@163.com>
2023-12-14 23:45:11 +0800

    update selectors

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,15 @@
 export const siteSelectors = {
-  bilibili: ['div.topic-panel', 'main.bili-feed4-layout', 'div.popular-container'],
-  douyin: ['div#waterFallScrollContainer', 'div.fullscreen_capture_feedback'],
+  bilibili: [
+    'div.topic-panel',
+    'main.bili-feed4-layout',
+    'div.popular-container',
+    'div.right-container-inner.scroll-sticky > div:last-child', // 推荐播放列表
+    'div#activity_vote', // 活动条
+  ],
+  douyin: [
+    'div#waterFallScrollContainer', // 首页
+    'div.page-recommend-container', // 推荐-单列
+  ],
+  youtube: ['ytd-two-column-browse-results-renderer[page-subtype="home"]'],
+  twitter: ['div[role="presentation"]:first-child', 'aside[aria-label="推荐关注"]'],
 };
