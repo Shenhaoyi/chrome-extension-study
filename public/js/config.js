02c2b70fd89af73da594176344672e2d919053a7
Shenhaoyi <shenhaoyi04@163.com>
2023-12-14 19:46:30 +0800

    屏蔽抖音的推荐

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,4 @@
 export const siteSelectors = {
   bilibili: ['aside.right', 'main.bili-feed4-layout', 'div.popular-container'],
+  douyin: ['div#waterFallScrollContainer', 'div.fullscreen_capture_feedback'],
 };
