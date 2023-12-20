d644aafdfe9a9bfca424b7fa131a850b9924e5f8
Shenhaoyi <shenhaoyi04@163.com>
2023-12-20 15:43:18 +0800

    新增需要隐藏的选择器

--- public/js/config.js
+++ public/js/config.js
@@ -3,12 +3,14 @@ export const siteSelectors = {
     'div.topic-panel',
     'main.bili-feed4-layout',
     'div.popular-container',
-    'div.right-container-inner.scroll-sticky > div:last-child', // 推荐播放列表
+    'div.right-container-inner.scroll-sticky > div:last-child > div#reco_list', // 推荐播放列表
+    'div.right-container-inner.scroll-sticky > div:last-child > div.pop-live-small-mode', // 推荐直播
     'div#activity_vote', // 活动条
+    'div.adblock-tips', // 顶部广告拦截提示
   ],
   douyin: [
     'div#waterFallScrollContainer', // 首页
-    'div.page-recommend-container', // 推荐-单列
+    'div#douyin-right-container > div#slidelist.fullscreen_capture_feedback', // 推荐-单列(也包含关注哪里的)
   ],
   youtube: ['ytd-two-column-browse-results-renderer[page-subtype="home"]'],
   twitter: ['div[role="presentation"]:first-child', 'aside[aria-label="推荐关注"]'],
