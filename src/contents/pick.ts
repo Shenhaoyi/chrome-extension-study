0638a5d024cbefc2cd925dd01bb2fcddf4ff903b
Shenhaoyi <shenhaoyi04@163.com>
2024-04-09 19:28:18 +0800

    引入 pick-dom-element

---
+++ src/contents/pick.ts
@@ -0,0 +1,9 @@
+import { picker } from "../utils/picker"
+
+// picker.start({
+//   onClick: (el) => {
+//     picker.stop()
+//     console.log("Picked: ====================")
+//     console.dir(el)
+//   }
+// })
---
+++ src/utils/picker.ts
@@ -0,0 +1,4 @@
+import { ElementPicker } from "pick-dom-element"
+
+const style = { borderColor: "#0000ff" }
+export const picker = new ElementPicker({ style })
