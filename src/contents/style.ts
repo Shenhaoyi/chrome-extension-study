f9e9d23abe93dbcba6a209c6100d5c9c657ff2ab
Shenhaoyi <shenhaoyi04@163.com>
2024-04-12 13:59:47 +0800

    eleduck 正文居中

--- src/contents/style.ts
+++ src/contents/style.ts
@@ -1,19 +1,7 @@
-import bilibili from 'url:../css/bilibili.css';
-import github from 'url:../css/github.css';
-import google from 'url:../css/google.css';
-import juejin from 'url:../css/juejin.css';
-import v2ex from 'url:../css/v2ex.css';
+import styleSrc from '~/css';
 import { enabled } from './mainSwitch';
 
-const styleSrc = {
-  bilibili,
-  v2ex,
-  juejin,
-  google,
-  github,
-};
-
-const loadStyle = async (filename) => {
+const loadStyle = async (filename: string) => {
   const link = document.createElement('link');
   link.type = 'text/css';
   link.rel = 'stylesheet';
@@ -23,16 +11,16 @@ const loadStyle = async (filename) => {
   container.appendChild(link);
 };
 
-// 需要加载的 css 文件名
-const cssFileNameList = ['bilibili', 'v2ex', 'juejin', 'github', 'google'];
-
 const init = async () => {
   // 判断是否启用
   if (!(await enabled)) {
     return;
   }
+  const url = window.location.href;
   // 匹配网站地址就加载
-  cssFileNameList.filter((item) => window.location.href.includes(item)).forEach(loadStyle);
+  Object.keys(styleSrc)
+    .filter((item) => url.includes(item))
+    .forEach(loadStyle);
 };
 
 init();
---
+++ src/css/index.ts
@@ -0,0 +1,15 @@
+import bilibili from 'url:./bilibili.css';
+import github from 'url:./github.css';
+import google from 'url:./google.css';
+import juejin from 'url:./juejin.css';
+import v2ex from 'url:./v2ex.css';
+import eleduck from 'url:./eleduck.css';
+
+export default {
+  bilibili,
+  v2ex,
+  juejin,
+  google,
+  github,
+  eleduck,
+};
