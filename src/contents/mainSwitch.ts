be3fa14162e8aa352e46d34270506e48984f73a2
Shenhaoyi <shenhaoyi04@163.com>
2024-04-14 23:42:10 +0800

    用withResolvers简化

--- src/contents/mainSwitch.ts
+++ src/contents/mainSwitch.ts
@@ -1,10 +1,11 @@
 import api from '~api';
 
-let _promise: (value: unknown) => void;
-export let enabled = new Promise((resolve) => (_promise = resolve));
+const { promise, resolve } = Promise.withResolvers();
 
 const init = async () => {
   const enabled = await api.mainSwitch.get();
-  _promise(enabled);
+  resolve(enabled);
 };
 init();
+
+export const enabled = promise;
