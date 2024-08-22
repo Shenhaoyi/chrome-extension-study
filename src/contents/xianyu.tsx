4b420580aefd7c7816b70facca47b63cda37da01
Shenhaoyi <shenhaoyi04@163.com>
2024-08-22 17:47:08 +0800

    支持刷新时间间隔设置

--- src/contents/xianyu.tsx
+++ src/contents/xianyu.tsx
@@ -1,7 +1,8 @@
 import type { PlasmoCSConfig } from 'plasmo';
 import cssText from 'data-text:~/contents/xianyu.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Button from 'antd/es/button';
+import { InputNumber, type InputNumberProps } from 'antd';
 
 export const config: PlasmoCSConfig = {
   matches: ['https://www.goofish.com/*'],
@@ -12,33 +13,40 @@ export const config: PlasmoCSConfig = {
 export const getStyle = () => {
   const style = document.createElement('style');
   style.textContent = cssText;
+  // TODO: antd样式引入
   return style;
 };
 
-const startRefresh = () => {
+const startRefresh = (sec: number) => {
   const id = setInterval(() => {
     const btn = document.querySelector('.search-price-confirm-button--I2ThavjG') as any;
     btn.click();
-  }, 5000);
+  }, sec * 1000);
   return id;
 };
 
 const PlasmoOverlay = () => {
   const [id, setId] = useState(null);
-  useEffect(() => {
-    setId(startRefresh());
-  }, []);
+  const [sec, setSec] = useState(5);
+
   const onClick = () => {
     if (id) {
       clearInterval(id);
       setId(null);
     } else {
-      const newId = startRefresh();
+      const newId = startRefresh(sec);
       setId(newId);
     }
   };
+
+  const onChange: InputNumberProps['onChange'] = (value) => {
+    clearInterval(id);
+    setId(null);
+    setSec(Number(value));
+  };
   return (
     <div className="hw-top">
+      <InputNumber min={5} max={100} defaultValue={sec} onChange={onChange} />
       <Button onClick={onClick}>{id ? '暂停' : '开始'}</Button>
     </div>
   );
